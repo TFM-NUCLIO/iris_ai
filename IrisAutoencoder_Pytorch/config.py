a9524f615fc5d13e5f98cea279aefbe59e918957
Ricardo Kaminski <ricardoskaminski@gmail.com>
2023-05-23 11:30:41 -0300

    treino #1

---
+++ IrisAutoencoder_Pytorch/config.py
@@ -0,0 +1,11 @@
+# Configurações gerais
+hidden_units = 156
+learning_rate = 0.001
+batch_size = 32
+num_epochs = 10
+threshold = 0.95
+
+# Caminhos dos arquivos dos dados
+df_X1_path = "train_data/df_X1.pkl"
+df_X2_path = "train_data/df_X2.pkl"
+df_X3_path = "train_data/df_X3.pkl"
