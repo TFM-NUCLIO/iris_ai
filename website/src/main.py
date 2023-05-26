39d8710acb1456b934e060ddb7bb24b59dcaea48
Rogério Kazuhiro Okabe <rogeriookabe@outlook.com>
2023-05-26 19:42:16 -0300

    painel

--- website/src/main.py
+++ website/src/main.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 import os
 
-app = Flask(__name__)
-
+app = Flask(
+  __name__,
+  template_folder='templates',
+  static_folder='static'
+)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
+    
+@app.route('/calculate',methods = ['POST', 'GET'])
+def calculate():
+    if request.method == 'POST':
+        value1 = request.form['value1']
+        value2 = request.form['value2']
+    else:
+        value1 = request.args.get('value1')
+        value2 = request.args.get('value2')
+    return "calculate " + str(value1) + str(value2)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port, threaded=True)
